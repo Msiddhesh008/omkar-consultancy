bc2bd2cdc570c9b1aa06e3fc3db76da948d89b02
Siddhesh More <msiddhesh008@gmail.com>
2023-04-15 15:08:47 +0530

    updated

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroTitle from '../Components/SubComponents/HeroTitle'
+import FeatureCard from '../Components/SubComponents/FeatureCard'
+import epfo from "../images/EPFO-Color.png"
+import ServiceCard from './SubComponents/ServiceCard'
+
 
 const Services = () => {
   return (
     <div id='service' className='container-fluid'>
-      <div className="container pt-5">
+      <div className="container pt-5 pb-5">
         <div className="row pt-5">
           <div className='col-12 pt-5'>
         <HeroTitle 
@@ -13,7 +17,98 @@ const Services = () => {
             h2="We Offer Services"
             />
             </div>
-            
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+
+            <div class="carousel-inner">
+
+              <div class="carousel-item  active">
+                <div className="w-100 row mx-auto d-flex justify-content-center">
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                </div>
+              </div>
+
+              
+              <div class="carousel-item ">
+                <div className="w-100 row mx-auto   d-flex justify-content-center">
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                </div>
+              </div>
+
+              
+              <div class="carousel-item">
+                <div className="w-100 row mx-auto  d-flex justify-content-center">
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                      <ServiceCard 
+                      title="Dummy"
+                      link={epfo}
+                      disc="Best Provident Fund / PF Registration Consultant for company's. We provide Monthly PF Compliance / Return Filling"
+                      />
+
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
           </div>
       </div>
       
---
+++ src/Components/SubComponents/ServiceCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ServiceCard = ({ link, title, disc}) => {
+  return (
+    
+    <div className="col-4 service-card pt-4 pb-4 ps-3 pe-3 text-center m-2">
+        <img src={link} alt="" width={100} />
+        <h3 className='mt-3'>{title}</h3>
+        <p>{disc}</p>
+        <Link to="#" style={{textDecoration:"none"}}><p className='featuredCardBtn' >More Details</p></Link>
+    
+    </div>
+  )
+}
+
+export default ServiceCard
